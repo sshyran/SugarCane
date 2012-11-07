6fc08cd09128ab6e3ce093a5ad6b48adb12d5fee
Gene Robinson <gene@thematictheme.com>
2012-11-07 15:20:18 -0500

    Cleaning up themelink filter
    Adding dummy theme url. Theme url needs to link to a page that is
    representative of the theme. Moved ableparris.com to becom the author
    attribution link.

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 // Add a link back to your Child Theme release page in the [theme-link] shortcode
 function childtheme_theme_link($themelink) {
-    return $themelink . ' &amp; <a class="Sugar Cane" href="http://ableparris.com/" title="A sweet Wordpress Theme" rel="designer">Sugar Cane by Able Parris</a>';
+    return $themelink . ' &amp; <a class="Sugar Cane" href="http://theme-url.somewhere" title="A sweet Wordpress Theme">Sugar Cane</a> by <a href="http://ableparris.com">Able Parris</a>';
 }
 add_filter('thematic_theme_link', 'childtheme_theme_link');
 ?>
